dd643e0d51381a5d32acc025dfa5d47b61c021e9
JuriJuriM <jurimudry@gmail.com>
2022-06-20 12:27:01 +0300

    add new object

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// Импортируйте компоненты
+import { doc } from "prettier";
+import main from "./main";
+main();
+
+console.log('Hi!');
+
+// Slider number three
+
+let range = document.querySelector('.slider-three__input');
+let rangeNum = document.querySelector('.slider-three__num');
+range.oninput = function () {
+    //console.log(this.value);
+    rangeNum.style.left = this.value + 'px';
+    rangeNum.innerHTML = this.value;
+}
---
+++ src/main.js
@@ -0,0 +1,6 @@
+// Импортируйте CSS-модуль 
+import classes from './main.scss';
+
+export default () => {
+    console.log(classes.main);
+}
